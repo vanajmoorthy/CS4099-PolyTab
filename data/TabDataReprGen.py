51a1fc883660ebfc83da4c5ccfffc50a1cb72063
Vanaj Moorthy <vanajmoorthy@gmail.com>
2024-02-23 12:55:07 +0000

    .

--- data/TabDataReprGen.py
+++ data/TabDataReprGen.py
@@ -104,20 +104,11 @@ class TabDataReprGen:
         if self.normalize:
             data = librosa.util.normalize(data)
         if self.downsample:
-            data = librosa.resample(data, self.sr_original, self.sr_downs)
+            data = librosa.resample(data, orig_sr=self.sr_original, target_sr=self.sr_downs)
             self.sr_curr = self.sr_downs
         if self.preproc_mode == "c":
             data = np.abs(librosa.cqt(data, hop_length=self.hop_length, sr=self.sr_curr, n_bins=self.cqt_n_bins, bins_per_octave=self.cqt_bins_per_octave))
-        elif self.preproc_mode == "m":
-            data = librosa.feature.melspectrogram(y=data, sr=self.sr_curr, n_fft=self.n_fft, hop_length=self.hop_length)
-        elif self.preproc_mode == "cm":
-            cqt = np.abs(librosa.cqt(data, hop_length=self.hop_length, sr=self.sr_curr, n_bins=self.cqt_n_bins, bins_per_octave=self.cqt_bins_per_octave))
-            mel = librosa.feature.melspectrogram(y=data, sr=self.sr_curr, n_fft=self.n_fft, hop_length=self.hop_length)
-            data = np.concatenate((cqt, mel), axis=0)
-        elif self.preproc_mode == "s":
-            data = np.abs(librosa.stft(data, n_fft=self.n_fft, hop_length=self.hop_length))
-        else:
-            print("Invalid representation mode.")
+        
 
         return data
 
