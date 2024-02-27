e9ac207b3aa83a74f290e11cb42f114549bb88f0
Vanaj Moorthy <vanajmoorthy@gmail.com>
2024-02-27 13:17:37 +0000

    upload

--- data/Parallel_TabDataReprGen.py
+++ data/Parallel_TabDataReprGen.py
@@ -1,17 +1,17 @@
 from TabDataReprGen import main
 from multiprocessing import Pool
+import sys
 
 # number of files to process overall
 num_filenames = 360
-modes = ["c", "m", "cm", "s"]
+modes = ["c","m","cm","s"]
 
-# Create a list of indices for filenames and multiply it by the number of modes
-filename_indices = list(range(num_filenames)) * len(modes)
-# Create a list that repeats each mode num_filenames times and concatenates them
-mode_list = [mode for mode in modes for _ in range(num_filenames)]
+filename_indices = list(range(num_filenames)) * 4
+mode_list = [modes[0]] * 360 + [modes[1]] * 360 + [modes[2]] * 360 + [modes[3]] * 360 
 
 if __name__ == "__main__":
-    # number of processes that will run simultaneously
-    with Pool(11) as pool:
-        # zip creates pairs of filename_indices and mode_list, which are passed to main
-        results = pool.starmap(main, [[index, mode] for index, mode in zip(filename_indices, mode_list)])
+    # number of processes will run simultaneously
+    pool = Pool(11)
+    inputval = list(zip(filename_indices, mode_list))
+    print(inputval)
+    results = pool.starmap(main, inputval)
