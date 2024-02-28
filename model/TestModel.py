5015406f6ac2096a798c593a2b5784ec89a2fa02
Vanaj Moorthy <vanajmoorthy@gmail.com>
2024-02-28 12:16:21 +0000

    fix model arch

--- model/TestModel.py
+++ model/TestModel.py
@@ -5,6 +5,13 @@ import librosa
 from scipy.io import wavfile
 import os
 
+from __future__ import print_function
+import keras
+import os
+from keras import backend as K
+from Metrics import *
+import tensorflow as tf
+
 # Define paths
 MODEL_WEIGHTS_PATH = 'model/saved/c 2024-02-27 151223/0/weights.h5'
 AUDIO_FILE_PATH = 'model/00_BN1-147-Gb_solo_mic.wav'
@@ -21,10 +28,31 @@ SR_ORIGINAL, data = wavfile.read(AUDIO_FILE_PATH)
 # Define your model architecture here
 
 
+def softmax_by_string(self, t):
+    sh = K.shape(t)
+    string_sm = []
+    for i in range(self.num_strings):
+        string_sm.append(K.expand_dims(K.softmax(t[:, i, :]), axis=1))
+    return K.concatenate(string_sm, axis=1)
+
+
+def catcross_by_string(self, target, output):
+    loss = 0
+    for i in range(self.num_strings):
+        loss += K.categorical_crossentropy(
+            target[:, i, :], output[:, i, :])
+    return loss
+
+
+def avg_acc(self, y_true, y_pred):
+    return K.mean(K.equal(K.argmax(y_true, axis=-1), K.argmax(y_pred, axis=-1)))
+
+
 def build_model(input_shape, num_strings, num_classes):
     model = Sequential()
     model.add(Conv2D(32, kernel_size=(3, 3),
-              activation='relu', input_shape=input_shape))
+                     activation='relu',
+                     input_shape=input_shape))
     model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -34,9 +62,11 @@ def build_model(input_shape, num_strings, num_classes):
     model.add(Dropout(0.5))
     model.add(Dense(num_classes * num_strings))  # no activation
     model.add(Reshape((num_strings, num_classes)))
-    model.add(Activation('softmax'))
+    model.add(Activation(softmax_by_string))
 
-    # model.compile(...)
+    model.compile(loss=catcross_by_string,
+                  optimizer=tf.keras.optimizers.Adadelta(),
+                  metrics=[avg_acc])
 
     return model
 
