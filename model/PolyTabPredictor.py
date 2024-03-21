58463296f6cccbd5736fedbc1d994331cf2e543a
Vanaj Moorthy <vanajmoorthy@gmail.com>
2024-03-21 15:01:01 +0000

    gd syntax error

--- model/PolyTabPredictor.py
+++ model/PolyTabPredictor.py
@@ -79,7 +79,8 @@ class PolyTabPredictor:
                 output_dir, f"{os.path.splitext(audio_filename)[0]}_tabs.txt")
             with open(output_file, "w") as f:
                 for frame_idx, tab_frame in enumerate(tabs):
-                    f.write(f"Frame {frame_idx}: {' '.join(map(str, tab_frame))}\n"
+                    f.write(f"Frame {frame_idx}: {
+                            ' '.join(map(str, tab_frame))}\n")
 
         return tabs
 
