001b8f03eda2466f60bdad8185895b04406e1805
Vanaj Moorthy <vanajmoorthy@gmail.com>
2024-03-26 13:15:38 +0000

    generate ground truth reprs

---
+++ data/GenerateTabs.py
@@ -0,0 +1,46 @@
+import os
+import numpy as np
+import librosa
+import jams
+
+data_path = "GuitarSet/audio/audio_mic"
+save_path = "ground_truth_tabs"
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+
+def convert_note_to_tab(note, string_midi):
+    # Conversion logic to find the fret number
+    fret = note - string_midi
+    if fret < 0 or fret > highest_fret:
+        return None
+    return fret
+
+def save_tab(file_name, annotations):
+    with open(os.path.join(save_path, f"{file_name}.txt"), "w") as f:
+        for string_annotations in annotations:
+            # Assuming `string_annotations` is a list of lists with fret numbers per frame
+            for frame in string_annotations:
+                f.write('\t'.join(str(fret) if fret is not None else '-' for fret in frame))
+                f.write('\n')  # Newline to separate frames
+
+# List of MIDI notes for standard guitar strings
+string_midi_pitches = [40, 45, 50, 55, 59, 64]
+highest_fret = 19
+
+for file_name in os.listdir(data_path):
+    if file_name.endswith(".jams"):
+        # Load JAMS annotation
+        jam = jams.load(os.path.join(data_path, file_name))
+        annotations = []
+        
+        for string_midi in string_midi_pitches:
+            # Get the annotations for each string
+            string_notes = jam.search(namespace='pitch_midi')[string_midi].data
+            string_annotations = []
+            for note in string_notes:
+                fret = convert_note_to_tab(note.value, string_midi)
+                string_annotations.append(fret)
+            annotations.append(string_annotations)
+        
+        # Save the tab for the song
+        save_tab(file_name.split('.')[0], annotations)
