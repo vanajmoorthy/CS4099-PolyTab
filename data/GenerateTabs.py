e23b6cec8f244f88f722d8425546155ca990a8bf
Vanaj Moorthy <vanajmoorthy@gmail.com>
2024-03-26 15:29:33 +0000

    debug

--- data/GenerateTabs.py
+++ data/GenerateTabs.py
@@ -8,44 +8,40 @@ save_path = "ground_truth_tabs"
 if not os.path.exists(save_path):
     os.makedirs(save_path)
 
-def convert_note_to_tab(note, string_midi):
-    # Conversion logic to find the fret number
-    fret = note - string_midi
-    if fret < 0 or fret > highest_fret:
-        return None
-    return fret
+# Conversion logic to find the fret number
+def convert_note_to_fret(midi_note, string_midi_pitch):
+    fret_number = int(round(midi_note)) - string_midi_pitch
+    if 0 <= fret_number <= highest_fret:
+        return str(fret_number)
+    return 'X'  # 'X' indicates a note that is not playable on this string
 
-def get_string_notes(annotations, string_midi):
-    string_notes = []
-    for note in annotations:
-        if note.value == string_midi:
-            string_notes.append(note)
-    return string_notes
+# Load JAMS annotation and convert to guitar tabs
+def process_annotations(file_name):
+    jam = jams.load(os.path.join(data_path, file_name))
+    tab_lines = [[] for _ in range(6)]  # one sublist for each string
 
-def save_tab(file_name, annotations):
-    with open(os.path.join(save_path, f"{file_name}.txt"), "w") as f:
-        for string_annotations in annotations:
-            # Assuming `string_annotations` is a list of lists with fret numbers per frame
-            for frame in string_annotations:
-                f.write('\t'.join(str(fret) if fret is not None else '-' for fret in frame))
-                f.write('\n')  # Newline to separate frames
+    for ann in jam.annotations['note_midi']:
+        for note in ann:
+            string_num = None
+            # Determine which string this note belongs to
+            for i, string_midi in enumerate(string_midi_pitches):
+                if abs(note.value - string_midi) < 3:  # threshold for string matching
+                    string_num = i
+                    break
+            if string_num is not None:
+                fret = convert_note_to_fret(note.value, string_midi_pitches[string_num])
+                tab_lines[string_num].append(fret)
+
+    # Write the tab to a text file, with each string on its own line
+    with open(os.path.join(save_path, f"{os.path.splitext(file_name)[0]}_tab.txt"), 'w') as fp:
+        for string_frets in tab_lines:
+            fp.write(' '.join(string_frets) + '\n')
 
 # List of MIDI notes for standard guitar strings
 string_midi_pitches = [40, 45, 50, 55, 59, 64]
 highest_fret = 19
 
+# Process all .jams files in the directory
 for file_name in os.listdir(data_path):
     if file_name.endswith(".jams"):
-        # Load JAMS annotation
-        jam = jams.load(os.path.join(data_path, file_name))
-        string_annotations = []
-        
-        for string_midi in string_midi_pitches:
-            # Get the annotations for each string
-            string_annotations = get_string_notes(jam.annotations['note_midi'][0], string_midi)
-            # print(string_annotations)
-            print(jam.annotations['note_midi'][2])
-
-        
-        # Save the tab for the song
-        save_tab(file_name.split('.')[0], string_annotations)
+        process_annotations(file_name)
