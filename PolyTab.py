8ff3e60059e724d725471d7400be7cda0d23b0e0
Vanaj Moorthy <vanajmoorthy@gmail.com>
2024-04-14 23:16:58 +0100

    Use correct loss

--- PolyTab.py
+++ PolyTab.py
@@ -1,8 +1,3 @@
-''' A CNN to classify 6 fret-string positions
-    at the frame level during guitar performance
-'''
-
-
 import os
 import datetime
 import tensorflow as tf
@@ -23,9 +18,11 @@ if gpus:
     try:
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
-        print(f"{len(gpus)} Physical GPUs, {len(tf.config.experimental.list_logical_devices('GPU'))} Logical GPUs")
+        print(f"{len(gpus)} Physical GPUs, {
+              len(tf.config.experimental.list_logical_devices('GPU'))} Logical GPUs")
     except RuntimeError as e:
         print(e)
+
 
 class PolyTab:
     def __init__(self, batch_size=128, epochs=8, con_win_size=9, spec_repr="c", data_path="./cqt/", id_file="id.csv", save_path="./model/saved/"):
@@ -40,22 +37,28 @@ class PolyTab:
         self.num_classes = 21
         self.num_strings = 6
         self.load_IDs()
-        self.save_folder = os.path.join(self.save_path, f"{self.spec_repr} {datetime.datetime.now().strftime('%Y-%m-%d %H%M%S')}/")
+        self.save_folder = os.path.join(self.save_path, f"{self.spec_repr} {
+                                        datetime.datetime.now().strftime('%Y-%m-%d %H%M%S')}/")
         os.makedirs(self.save_folder, exist_ok=True)
         self.log_file = os.path.join(self.save_folder, "log.txt")
-        self.metrics = {"pp": [], "pr": [], "pf": [], "tp": [], "tr": [], "tf": [], "tdr": [], "data": ["g0", "g1", "g2", "g3", "g4", "g5", "mean", "std dev"]}
+        self.metrics = {"pp": [], "pr": [], "pf": [], "tp": [], "tr": [], "tf": [
+        ], "tdr": [], "data": ["g0", "g1", "g2", "g3", "g4", "g5", "mean", "std dev"]}
 
     def load_IDs(self):
-        self.list_IDs = list(pd.read_csv(os.path.join(self.data_path, self.id_file), header=None)[0])
+        self.list_IDs = list(pd.read_csv(os.path.join(
+            self.data_path, self.id_file), header=None)[0])
 
     def partition_data(self, data_split):
         self.partition = {"training": [], "validation": []}
         for ID in self.list_IDs:
             guitarist = int(ID.split("_")[0])
-            self.partition["validation" if guitarist == data_split else "training"].append(ID)
+            self.partition["validation" if guitarist ==
+                           data_split else "training"].append(ID)
 
-        self.training_generator = DataGenerator(self.partition['training'], data_path=self.data_path, batch_size=self.batch_size, shuffle=True, spec_repr=self.spec_repr, con_win_size=self.con_win_size)
-        self.validation_generator = DataGenerator(self.partition['validation'], data_path=self.data_path, batch_size=len(self.partition['validation']), shuffle=False, spec_repr=self.spec_repr, con_win_size=self.con_win_size)
+        self.training_generator = DataGenerator(self.partition['training'], data_path=self.data_path,
+                                                batch_size=self.batch_size, shuffle=True, spec_repr=self.spec_repr, con_win_size=self.con_win_size)
+        self.validation_generator = DataGenerator(self.partition['validation'], data_path=self.data_path, batch_size=len(
+            self.partition['validation']), shuffle=False, spec_repr=self.spec_repr, con_win_size=self.con_win_size)
 
         self.split_folder = os.path.join(self.save_folder, str(data_split))
         os.makedirs(self.split_folder, exist_ok=True)
@@ -65,7 +68,6 @@ class PolyTab:
         for i in range(self.num_strings):
             string_sm.append(tf.expand_dims(tf.nn.softmax(t[:, i, :]), axis=1))
         return tf.concat(string_sm, axis=1)
-    
 
     # def catcross_by_string(self, target, output):
     #     loss = 0
@@ -113,7 +115,8 @@ class PolyTab:
 
     def build_model(self):
         model = Sequential([
-            Conv2D(32, (3, 3), activation='relu', input_shape=self.input_shape),
+            Conv2D(32, (3, 3), activation='relu',
+                   input_shape=self.input_shape),
             Conv2D(64, (3, 3), activation='relu'),
             Conv2D(64, (3, 3), activation='relu'),
             MaxPooling2D((2, 2)),
@@ -135,7 +138,8 @@ class PolyTab:
         #               optimizer=tf.keras.optimizers.Adadelta(),
         #               metrics=[self.avg_acc])
 
-        model.compile(optimizer=AdamW(learning_rate=0.01, weight_decay=1e-4), metrics=['accuracy'], loss=loss_layer.call)
+        model.compile(optimizer=AdamW(learning_rate=0.01, weight_decay=1e-4),
+                      metrics=['accuracy'], loss=loss_layer.call)
         self.model = model
 
     def train(self):
@@ -144,9 +148,11 @@ class PolyTab:
         tensorboard_callback = TensorBoard(log_dir=tensorboard_dir)
 
         checkpoint_filepath = os.path.join(self.save_folder, 'checkpoint.h5')
-        model_checkpoint_callback = ModelCheckpoint(filepath=checkpoint_filepath, save_weights_only=True, monitor='loss', mode='min', save_best_only=True)
+        model_checkpoint_callback = ModelCheckpoint(
+            filepath=checkpoint_filepath, save_weights_only=True, monitor='loss', mode='min', save_best_only=True)
 
-        self.model.fit(self.training_generator, epochs=self.epochs, verbose=1, callbacks=[model_checkpoint_callback, tensorboard_callback], use_multiprocessing=True, workers=9)
+        self.model.fit(self.training_generator, epochs=self.epochs, verbose=1, callbacks=[
+                       model_checkpoint_callback, tensorboard_callback], use_multiprocessing=True, workers=9)
 
     def save_weights(self):
         self.model.save_weights(os.path.join(self.split_folder, "weights.h5"))
@@ -156,7 +162,8 @@ class PolyTab:
         self.y_pred = self.model.predict(self.X_test)
 
     def save_predictions(self):
-        np.savez(os.path.join(self.split_folder, "predictions.npz"), y_pred=self.y_pred, y_gt=self.y_gt)
+        np.savez(os.path.join(self.split_folder, "predictions.npz"),
+                 y_pred=self.y_pred, y_gt=self.y_gt)
 
     def evaluate(self):
         self.metrics["pp"].append(pitch_precision(self.y_pred, self.y_gt))
@@ -178,6 +185,7 @@ class PolyTab:
         output["data"] = self.metrics["data"]
         df = pd.DataFrame.from_dict(output)
         df.to_csv(os.path.join(self.save_folder, "results.csv"))
+
 
 if __name__ == '__main__':
     polytab = PolyTab()
